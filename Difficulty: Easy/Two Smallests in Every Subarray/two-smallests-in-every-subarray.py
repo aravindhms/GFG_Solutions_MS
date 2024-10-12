0d236ee23965de4bef4b539a21a5e7ba107eefac
Aravindh MS <aravindhms1@gmail.com>
2024-10-12 21:37:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Smallests in Every Subarray/two-smallests-in-every-subarray.py
@@ -0,0 +1,33 @@
+class Solution:
+    def pairWithMaxSum(self, arr):
+        #code here
+        if len(arr) < 2:
+            return -1
+
+        max_sum = -1
+
+        for i in range(len(arr) - 1):
+            smallest = min(arr[i], arr[i + 1])
+            second_smallest = max(arr[i], arr[i + 1])
+            current_sum = smallest + second_smallest
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().strip().split("\n")
+
+    t = int(data[0])
+    lines = data[1:]
+
+    for line in lines:
+        s = list(map(int, line.strip().split()))
+        solution = Solution()
+        res = solution.pairWithMaxSum(s)
+        print(res)
+
+# } Driver Code Ends
